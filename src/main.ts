54f11d8c0bc177d90ae81a45a854838bb2424ea1
Gordon McLean <mcleangm@telus.net>
2020-03-01 10:19:55 -0700

    l49 configure swagger with enstjs app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+// swagger api
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+
+// pipes
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // config swagger options
+  const options = new DocumentBuilder()
+    .setTitle('Auth Example')
+    .setDescription('JSWT Authentication demo with MongoDB')
+    .setVersion('1.0')
+    .addTag('users')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options);
+  // setup swagger module
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
